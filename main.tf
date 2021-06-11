ecb73b7a548bee1fc6675a210305a781ec9ae786
mathieu-monselet <71387773+mathieu-monselet@users.noreply.github.com>
2021-06-11 11:13:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-vault"
+      name = "mathieu-m-vault"
     }
   }
   required_providers {
